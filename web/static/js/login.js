418205edabdc922f2e15a46d5a70cf72b5d67513
giordano.alvitez@utec.edu.pe <giordano.alvitez@utec.edu.pe>
2019-11-08 10:31:16 +0000

    .

--- web/static/js/login.js
+++ web/static/js/login.js
@@ -1,11 +1,13 @@
 var btnLogin=document.getElementById('btnSubmit')
-var socket=io.connect(location.protocol+'//'+document.domain+':'+location.port)
+/*var socket=io.connect(location.protocol+'//'+document.domain+':'+location.port)
 btnLogin.addEventListener('click',function(){
     var username = $('#username').val();
     var credentials = {'username':username};
     socket.emit('addUserToRoom', credentials);
 });
+*/
 
+/*
 $(window).on('keydown', function(e) {
   if (e.which == 13) {
     console.log("Entro para setear usuario a join room ")
@@ -16,3 +18,4 @@ $(window).on('keydown', function(e) {
     return false;
   }
 });
+*/
